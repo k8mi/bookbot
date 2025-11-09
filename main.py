1c10b39aae8fb2998b8b5532fb8ed19ce563b252
k8mi <kate.miyasaki@gmail.com>
2025-11-08 16:46:14 -0800

    Added more book files and used sys.argv to take argument in program for book file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_word_count
 from stats import get_character_count
 from stats import sort_dictionary
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,22 +10,25 @@ def get_book_text(filepath):
 def sort_on(dictionary):
     return dictionary["num"]
 def main():
-    # testing get_book_text function
-    # frankenstein_text = get_book_text("books/frankenstein.txt")
-    # print(frankenstein_text)
-    frankenstein_path = "books/frankenstein.txt"
-    frankenstein_text = get_book_text("books/frankenstein.txt")
+
+    if(len(sys.argv)<2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+
+    book_text = get_book_text(book_path)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at " + frankenstein_path + "...")
+    print("Analyzing book found at " + book_path + "...")
     print("----------- Word Count ----------")
-    print(f"Found {get_word_count(frankenstein_text)} total words")
-    frankenstein_chars = get_character_count(frankenstein_text)
-    sorted_frankenstein_chars = sort_dictionary(frankenstein_chars)
+    print(f"Found {get_word_count(book_text)} total words")
+    book_chars = get_character_count(book_text)
+    sorted_book_chars = sort_dictionary(book_chars)
     print("--------- Character Count -------")
 
-    for i in range(0,len(sorted_frankenstein_chars)-1):
-        if sorted_frankenstein_chars[i]["char"].isalpha():
-            print(sorted_frankenstein_chars[i]["char"] + ": " + str(sorted_frankenstein_chars[i]["num"]))
+    for i in range(0,len(sorted_book_chars)-1):
+        if sorted_book_chars[i]["char"].isalpha():
+            print(sorted_book_chars[i]["char"] + ": " + str(sorted_book_chars[i]["num"]))
     print("============= END ===============")
 
 
